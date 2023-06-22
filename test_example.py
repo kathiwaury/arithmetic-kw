2a694d67f6fa37a46a29fedb70106a08432877c8
Waury <kwy700@login3.cm.cluster>
2023-06-22 12:15:11 +0200

    Add test functions for subtract

--- test_example.py
+++ test_example.py
@@ -4,3 +4,7 @@ def test_add():
     assert add(0, 0) == 0
     assert add(3, 5) == 8
     assert add(-2, 3) == 1
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(10, 1) == 9
